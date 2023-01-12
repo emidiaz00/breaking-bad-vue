91ae6fd87718660285c0f581a1a31f6c2a9cf825
ediaz-wallypos <emildsn00@gmail.com>
2023-01-12 09:34:46 -0300

    add image background

--- src/components/QuoteCard.vue
+++ src/components/QuoteCard.vue
@@ -1,11 +1,11 @@
 <template>
-    <div v-for="quote in quotes" :key="quote.id">
+    <div classname="background-img" v-for="quote in quotes" :key="quote.id">
         <ul>
-            <li>{{ quotes.quote }}</li>
-            <li>{{ quotes.author }}</li>
+            <li>{{ quote.quote }}</li>
+            <li>{{ quote.author }}</li>
         </ul>
     </div>
-    <button @click="getRandomQuote">
+    <button @click="getRandomQuote" class="btn btn-secondary">
         Get Quote
     </button>
 </template>
@@ -20,11 +20,11 @@ export default {
     },
     methods: {
         getRandomQuote() {
-            const id = Math.floor(Math.random() * 101);
+            const id = Math.floor(Math.random() + 1);
             fetch(`https://api.breakingbadquotes.xyz/v1/quotes/${id}`)
             .then(response => response.json())
             .then(data => {
-                this.quotes = data[0];
+                this.quotes = data;
             });
         }
     }
@@ -32,5 +32,18 @@ export default {
 </script>
 
 <style>
+
+li {
+    list-style: none;
+    color: #fff;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-size: 24px;
+}
+body {
+    background-image: url('../assets/thumb-1920-851514.jpg');
+    background-size: cover; /* establece el tamaño de la imagen para cubrir todo el elemento */
+    background-repeat: no-repeat; /* evita que la imagen se repita */
+}
 
 </style>
